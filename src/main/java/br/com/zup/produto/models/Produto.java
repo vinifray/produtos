4b4fcf8a26e8033e81606e464ccb5f147342008d
Vinícius <vinicius.raymundo@zup.com.br>
2021-03-04 14:18:53 -0300

    Adicionar model de produto

---
+++ src/main/java/br/com/zup/produto/models/Produto.java
@@ -0,0 +1,43 @@
+package br.com.zup.produto.models;
+
+public class Produto {
+    private String nome;
+    private String descricao;
+    private int quantidade;
+    private double preco;
+
+    public Produto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
